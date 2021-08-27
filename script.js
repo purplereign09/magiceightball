126808aebdc42e292c7c7781cc98f7b17f2cc032
Patty Baraibar <pattybaraibar@msn.com>
2021-08-26 20:44:47 -0500

    added border

--- script.js
+++ script.js
@@ -1,61 +1,31 @@
-$(document).ready(function(){
+let answers = [
+	'Seems unlikely.',
+	'Not a chance.',
+	'In your dreams.',
+	'Get real, kid.',
+	'Absolutely.',
+	'Maybe.',
+	'It is certain.',
+	'Ask me later.',
+	'Chances seem good.',
+	'I don\'t know, I\'m just a stupid piece of plastic.',
+	'Ask again later.',
+	'Signs point to yes.',
+	'No.',
+	'Yes.',
+	'Nope.',
+	'Don\'t count on it.',
+	'Is the Pope Catholic?',
+	'Does a one legged duck swim in a circle?'
+]
 
-    
-
-});
-
-$(".clickBttn").click(function(){
-    //loop through an array of answers from switch statement
-    //append answers to DOM box
-    $(".answerBox").append("<p></p>");
-
-});
-
-let userName = 'Jane';
-let userQuestion = 'What is my future?';
-let randomNumber = Math.floor(Math.random() * 8);
-let eightBall = '';
-
-
-
-userName ? console.log(`Hello ${userName}!`) : console.log('Hello!');
-
-console.log(`The user asked: ${userQuestion}`);
-
-
-
-switch (randomNumber) {
-    case 0:
-        eightBall = 'It is certain';
-        break;
-    case 1:
-        eightBall = 'It is decidedly so';
-        break;
-    case 2:
-        eightBall = 'Reply hazy try again';
-        break;
-    case 3:
-        eightBall = 'Cannot predict now';
-        break;
-    case 4:
-        eightBall = 'Do not count on it';
-        break;
-    case 5:
-        eightBall = 'My sources say no';
-        break;
-    case 6:
-        eightBall = 'Outlook not so good'
-        break;
-    case 7:
-        eightBall = 'Signs point to yes'
-        break;
-    case 8:
-        eightBall = 'Definitely Not';
-        break;
-    default:
-        eightBall = 'You win the lottery!';
-        break;
+let displayAnswer = function()
+{
+	let index = Math.floor(Math.random() * answers.length);
+	let answer = answers[index];
+	let element = document.getElementById( 'answers' );
+	element.style.display = 'inline-block';
+	element.innerHTML = '<br><br>' + answer;
 }
 
-console.log(`The eight ball answered: ${eightBall}`);
 
